9c6acda73c702e16b3f826736eb73bfa5cacc2fc
arun <as2977@njit.edu>
2018-01-14 02:37:40 -0500

    React router part completed

--- src/App.js
+++ src/App.js
@@ -3,38 +3,27 @@ import React from 'react'
 import './App.css'
 import SearchPage from './SearchPage'
 import MainPage from './MainPage'
+import { Route } from 'react-router-dom'
 
 class BooksApp extends React.Component {
-  state = {
+ 
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
-  }
   
-  handleChange = () => {
-    this.setState({ showSearchPage: true })
-  }
-
-    handleChange2 = () => {
-    this.setState({ showSearchPage: false })
-  }
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchPage
-            onCli2 = {this.handleChange2}
-          />
-        ) : (
-          <MainPage
-            onCli = {this.handleChange}
-          />
-        )}
+        <Route exact path="/" render={() => (
+            <MainPage/>        
+          )}/>  
+        <Route path="/searchpage"  render={() => (
+            <SearchPage/>
+          )}/> 
       </div>
     )
   }
--- src/MainPage.js
+++ src/MainPage.js
@@ -1,4 +1,5 @@
 import  React, { Component } from 'react'
+import  { Link } from 'react-router-dom'
 
 class MainPage extends Component{
 	render(){
@@ -159,7 +160,10 @@ class MainPage extends Component{
               </div>
             </div>
             <div className="open-search">
-              <a onClick={() => this.props.onCli()}>Add a book</a>
+              <Link 
+                to ="/searchpage"
+                //onClick={() => this.props.onCli()}
+              >Add a book</Link>
             </div>
           </div>
 
