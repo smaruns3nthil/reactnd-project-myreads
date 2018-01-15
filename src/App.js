e92a01faeb71ebe235183e2c464ab0157e274bbb
arun <as2977@njit.edu>
2018-01-15 13:03:59 -0500

    Displayed data obtained using the getall function of booksapi.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import React,{ Component } from 'react'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchPage from './SearchPage'
 import MainPage from './MainPage'
@@ -7,21 +7,30 @@ import { Route } from 'react-router-dom'
 
 class BooksApp extends React.Component {
  
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
+   state = {
+    Books: [],
+  }
   
+  componentDidMount() {
+    BooksAPI.getAll()
+    .then((output) => {
+      this.setState({Books:output})
+      console.log(this.state.Books)
+    })
+  } 
+
 
   render() {
     return (
+
       <div className="app">
+        
         <Route exact path="/" render={() => (
-            <MainPage/>        
+            <MainPage
+            books={this.state.Books}        
+            />        
           )}/>  
-        <Route path="/searchpage"  render={() => (
+        <Route path="/search"  render={() => (
             <SearchPage/>
           )}/> 
       </div>
---
+++ src/ListBooks.js
@@ -0,0 +1,35 @@
+import  React, { Component } from 'react'
+
+
+
+class ListBooks extends Component{
+
+	render(){
+	// console.log('Props',this.props)
+		return(
+			<ol className="books-grid">
+				{this.props.books.map((book) => (
+	                      <li key={book.id}>
+	                        <div className="book">
+	                          <div className="book-top">
+	                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+	                            <div className="book-shelf-changer">
+	                              <select value={book.shelf}>
+	                                <option value="none" disabled>Move to...</option>
+	                                <option value="currentlyReading">Currently Reading</option>
+	                                <option value="wantToRead">Want to Read</option>
+	                                <option value="read">Read</option>
+	                                <option value="none">None</option>
+	                              </select>
+	                            </div>
+	                          </div>
+	                          <div className="book-title">{book.title}</div>
+	                          <div className="book-authors">{book.authors}</div>
+	                        </div>
+	                      </li>
+					))}
+	                    </ol>
+	    )
+	}
+}
+export default ListBooks
