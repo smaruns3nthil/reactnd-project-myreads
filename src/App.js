39c08b933cbf1e0b66084e1e581b7f3299b24034
arun <as2977@njit.edu>
2018-01-22 12:09:54 -0500

    Finish touches and updated readme file

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ class BooksApp extends React.Component {
           })
           if(!book.shelf)
           {
-          book.shelf='non'
+          book.shelf='none'
           }
           return book
         })
